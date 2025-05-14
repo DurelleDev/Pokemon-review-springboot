b4d8bf35379883a96d8a805d400673b3d22d6b02
Durelle Abdul <Durelle.abdul.07@gmail.com>
2025-05-14 16:54:08 +0200

    Added models
    Added the 2 main models for the project

---
+++ src/main/java/com/pokemonreview/api/models/Pokemon.java
@@ -0,0 +1,12 @@
+package com.pokemonreview.api.models;
+
+import lombok.Data;
+import lombok.Setter;
+
+@Data
+public class Pokemon {
+    private int id;
+    private String name;
+    private String type;
+
+}
