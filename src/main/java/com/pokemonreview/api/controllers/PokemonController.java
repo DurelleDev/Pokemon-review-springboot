25b34ab3300e1ff1f5f9cd7551b49cafa7ee21c3
Durelle <47389740+DurelleDev@users.noreply.github.com>
2025-05-16 14:31:53 +0200

    Added Pokémon Controller

---
+++ src/main/java/com/pokemonreview/api/controllers/PokemonController.java
@@ -0,0 +1,45 @@
+package com.pokemonreview.api.controllers;
+
+import com.pokemonreview.api.models.Pokemon;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/")
+public class PokemonController {
+
+    @GetMapping("pokemon")
+    public ResponseEntity<List<Pokemon>> getAllPokemons(){
+        List<Pokemon> pokemons = new ArrayList<>();
+
+        pokemons.add(new Pokemon(1,"Squirtle", "Water"));
+        pokemons.add(new Pokemon(2,"Pikachu", "Electric"));
+        pokemons.add(new Pokemon(3,"Charmander", "Fire"));
+
+        return ResponseEntity.ok(pokemons);
+    }
+
+    @GetMapping("pokemon/{id}")
+    public Pokemon pokemonDetail(@PathVariable int id){
+        return new Pokemon(id, "Squirtle", "Water");
+    }
+
+    @PostMapping("pokemon/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Pokemon> createPokemon(@RequestBody Pokemon pokemon){
+
+       System.out.println(pokemon.getName());
+       System.out.println(pokemon.getType());
+
+       return new ResponseEntity<>(pokemon, HttpStatus.CREATED);
+
+   }
+
+
+
+}
+
--- src/main/java/com/pokemonreview/api/models/Pokemon.java
+++ src/main/java/com/pokemonreview/api/models/Pokemon.java
@@ -1,10 +1,15 @@
 package com.pokemonreview.api.models;
 
-import lombok.Data;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Pokemon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String type;
