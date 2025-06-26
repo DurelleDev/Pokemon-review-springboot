c3139f48b58874aab5179c83bf45d24500ddb7ff
Durelle <47389740+DurelleDev@users.noreply.github.com>
2025-06-26 16:33:56 +0200

    Built the global exception handling for the application

---
+++ src/main/java/com/pokemonreview/api/exceptions/PokemonNotFoundException.java
@@ -0,0 +1,13 @@
+package com.pokemonreview.api.exceptions;
+
+import java.io.Serial;
+
+public class PokemonNotFoundException extends RuntimeException{
+    @Serial
+    private static final long serialVersionUID = 1;
+
+    public PokemonNotFoundException(String message){
+        super(message);
+    }
+
+}
